68ebda08029c6cb1737047049a122712085b2db3
pablonataniel76 <pablo.nataniel76@gmail.com>
2020-11-04 16:32:04 -0400

    before redux

--- src/containers/Register/index.js
+++ src/containers/Register/index.js
@@ -1,13 +1,32 @@
-import { Button, notification } from 'antd';
+import { Button, Cascader, notification, Select } from 'antd';
 import { Input, Form } from 'antd';
 import {Fragment, useEffect} from 'react';
 import React, {useReducer} from 'react';
+import { Option, OptGroup } from 'antd/lib/mentions';
+//
+const options = [
+  {
+    label: "Bolivia",
+    value: "bolivia",
+  },
+  {
+    label: "Peru",
+    value: "peru",
+  },
+  {
+    label: "Colombia",
+    value: "colombia",
+  },
+];
+
 //
 const initialState = {
   name: '',
   lastname: '',
+  ci: '',
   age: '',
-  email: '',
+  country: '',
+  city: '',
 }
 
 function reducer(state, {field, value}) {
@@ -29,14 +48,20 @@ const Formulario = () => {
     })
   }
 
-  const { name, lastname, age, email } = state
+  const { name, lastname, ci, age, country, city } = state
   console.log(state)
 //
     const openNotification = () => {
         notification.open({
           message: 'Nuevo usuario',
           description:
-            `El usuario ${state.name} ${state.lastname} ha sido registrado`,
+            `El usuario ${state.name} 
+                        ${state.lastname} 
+                        ${state.ci} 
+                        ${state.age} 
+                        ${state.country} 
+                        ${state.city}  
+            ha sido registrado`,
           onClick: () => {
             console.log(state.name);
           },
@@ -49,7 +74,9 @@ const Formulario = () => {
             console.log("Se desmontó el componente register");
         }
     }, [])
+    
 
+    
     return (
         <Fragment>
             <h1>Registrarse</h1>
@@ -62,26 +89,47 @@ const Formulario = () => {
                 </Form.Item>
 
                 <Form.Item
-                label="Lastname"
+                label="Apellido"
                 rules={[{ required: true, message: 'Please input your lastname!' }]}
                 >
                     <Input name="lastname" value={lastname} onChange={onChange}/>
                 </Form.Item>
 
                 <Form.Item
-                label="Age"
+                label="CI"
+                rules={[{ required: true, message: 'Please input your ci!' }]}
+                >
+                    <Input name="ci" value={ci} onChange={onChange}/>
+                </Form.Item>
+
+                <Form.Item
+                label="Edad"
                 rules={[{ required: true, message: 'Please input your age!' }]}
                 >
                     <Input name="age" value={age} onChange={onChange}/>
                 </Form.Item>
-
+              
                 <Form.Item
-                label="Email"
-                rules={[{ required: true, message: 'Please input your email!' }]}
+                  label="Pais"
                 >
-                    <Input name="email" value={email} onChange={onChange}/>
+                  
+                  <select name="country" value={country} onChange={onChange}>
+                    {options.map((option) => (
+                      <option value={option.value}>{option.label}</option>
+                    ))}
+                  </select>
+                  
+
                 </Form.Item>
+                <Form.Item
+                label="Ciudad"
+                rules={[{ required: true, message: 'Please input your city!' }]}
+                >
+                    <Input name="city" value={city} onChange={onChange}/>
+                </Form.Item>
+
                 <Button type="submit" className="btn btn-primary" onClick={openNotification} >Enviar</Button>
+                <Button type="" className="btn btn-primary" onClick={openNotification} >Iniciar Sesion</Button>
             </Form>
         </Fragment>
     );
